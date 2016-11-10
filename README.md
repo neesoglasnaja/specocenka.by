5839672e82024533c8ba02dcba9dca98e26977a4
Aleksandra Shinkevich <neesa.web@tut.by>
2016-11-10 22:36:35 +0300

    git init

---
+++ README.md
@@ -0,0 +1,15 @@
+# 1. View the site
+Open `index.html` in the browser and view pages
+
+# 2. Developer mode
+
+__Insure you have installed on your computer:__
+
+* [Node.js](https://nodejs.org/en/download/) and [path variables](http://stackoverflow.com/questions/8278143/node-js-how-to-run-node-command-from-any-path)
+* [Gulp](http://gulpjs.com/) (`npm i gulp -g`)
+
+__Console commands to run the project:__
+
+1. Install all dependenсies: `npm i`
+2. Run gulp watcher: `gulp`
+3. Enjoy the power :)
---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp'),
+    jade = require('gulp-jade'),
+    watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+var postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer');
+var csso = require('gulp-csso');
+
+var config = {
+    css: './src/styles/partials/*.css',
+    mainCss: './src/styles/main.css',
+    jade: './src/*.jade',
+    js: './src/js/*.js',
+    images: './src/images/*',
+    html: "./*.html",
+    dist: "./public"
+};
+gulp.task('jade', function() {
+    return gulp.src(config.jade)
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest(config.dist))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('css', function() {
+    return gulp.src(config.mainCss)
+        .pipe(
+            postcss([
+                require("postcss-easy-import"),
+                require('precss')({}),
+                require('postcss-calc')({warnWhenCannotResolve: true}),
+                autoprefixer({ browsers: ['last 2 versions'] })
+            ])
+        )
+        .pipe(csso({
+            restructure: false,
+            sourceMap: true,
+            debug: true
+        }))
+        .pipe(gulp.dest(config.dist + '/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('js', function() {
+    return gulp.src(config.js)
+        .pipe(gulp.dest(config.dist + '/js'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', function() {
+    browserSync.init({
+        server: {
+            baseDir: config.dist
+        }
+    });
+    gulp.watch(config.jade, function(event) {
+        gulp.run('jade');
+    });
+    gulp.watch([config.css, config.mainCss], function(event) {
+        gulp.run('css');
+    });
+    gulp.watch(config.js, function(event) {
+        gulp.run('js');
+    });
+    gulp.watch(config.html).on('change', browserSync.reload);
+});
