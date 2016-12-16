c74833e2e67bf3cd53224833509f269c0c6f7790
Aleksandra Shinkevich <neesa.web@tut.by>
2016-12-16 18:48:30 +0300

    add breadcrumbs && do not init slider for slider.length <=1

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -2,16 +2,17 @@ var Slider = {
     init: function() {
         var slides = document.querySelectorAll('.slides--item');
         var currentIndex = slides.length - 1;
-
-        setInterval(function() {
-            slides[currentIndex].setAttribute("style", "opacity: 0");
-            currentIndex--;
-            if (currentIndex == -1) {
-                currentIndex = slides.length - 1;
-                for (var i = 0; i < slides.length; i++) {
-                    slides[i].setAttribute("style", "");
+        if (currentIndex > 0) {
+            setInterval(function() {
+                slides[currentIndex].setAttribute("style", "opacity: 0");
+                currentIndex--;
+                if (currentIndex == -1) {
+                    currentIndex = slides.length - 1;
+                    for (var i = 0; i < slides.length; i++) {
+                        slides[i].setAttribute("style", "");
+                    }
                 }
-            }
-        }, 4000);
+            }, 4000);
+        }
     }
 }
